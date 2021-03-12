877e856b900e343a39ecc31613398754df5b9eb4
Eventuri7 <crosser231@gmail.com>
2021-03-12 16:57:06 +0200

    hide.py update

--- lab1/hide.py
+++ lab1/hide.py
@@ -1,27 +1,32 @@
 f = open("container.txt", "r")
-text = f.read()
+text_container = f.read()
+f.close()
 
-secret = "hello"
+secret_message = "hello"
 
 onespace = " "
 twospace = "  "
 
-result = ""
+encoded_message = ""
 
-# str to bin
-a = ' '.join(format(ord(x), 'b') for x in secret)
-binc = "".join(a.split()) # remove whitespaces
-print(binc)
+wordList = text_container.split() 
 
-# list of words
-wordList = text.split() 
+def str2bin():
+	binary_secret = ' '.join(format(ord(x), 'b') for x in secret_message)
+	split_binary_secret = "".join(binary_secret.split()) # remove whitespaces
+	return split_binary_secret
 
+print(str2bin())
 
-for i in range(len(binc)):
-	if binc[i] == '1':
-		result += wordList[i] + twospace
-	else:
-		result += wordList[i] + onespace
+split_binary_secret = str2bin()
 
-print(result)
+def encode_message(split_binary_secret, encoded_message):
+	for i in range(len(split_binary_secret)):
+		if split_binary_secret[i] == '1':
+			encoded_message += wordList[i] + twospace
+		else:
+			encoded_message += wordList[i] + onespace
+	return encoded_message
+
+print(encode_message(split_binary_secret, encoded_message))
 
