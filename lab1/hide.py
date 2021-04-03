87fb5ba25fa7063191f944ba5c86e5eef5d6804b
Eventuri7 <crosser231@gmail.com>
2021-04-03 10:38:45 +0300

    bug fix

--- lab1/hide.py
+++ lab1/hide.py
@@ -12,21 +12,26 @@ encoded_message = ""
 wordList = text_container.split() 
 
 def str2bin():
-	binary_secret = ' '.join(format(ord(x), 'b') for x in secret_message)
-	split_binary_secret = "".join(binary_secret.split()) # remove whitespaces
-	return split_binary_secret
+	binary_secret = ' '.join(format(ord(x), '08b') for x in secret_message)
+	return binary_secret
 
 print(str2bin())
 
-split_binary_secret = str2bin()
+binary_secret = str2bin()
 
-def encode_message(split_binary_secret, encoded_message):
-	for i in range(len(split_binary_secret)):
-		if split_binary_secret[i] == '1':
+def encode_message(binary_secret, encoded_message):
+	for i in range(len(binary_secret)):
+		if binary_secret[i] == '1':
 			encoded_message += wordList[i] + twospace
 		else:
 			encoded_message += wordList[i] + onespace
 	return encoded_message
 
-print(encode_message(split_binary_secret, encoded_message))
+print(encode_message(binary_secret, encoded_message))
+
+encoded_message = encode_message(binary_secret, encoded_message)
+
+result = open("encoded_message.txt", "w")
+result.write(encoded_message)
+result.close()
 
